c4225d1e1f7566272ca8f7c9a4fb275fc072932d
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-10-31 09:20:01 -0400

    linting

--- shared/shared.lua
+++ shared/shared.lua
@@ -3,5 +3,6 @@ Config = Config or {}
 Config.Core = 'qb-core'
 
 -- Time in miliseconds to install the harness (Progress Bar)
-Config.InstallTime = 10000 
+Config.InstallTime = 10000
+-- Harness item name
 Config.Harness = 'harness'
