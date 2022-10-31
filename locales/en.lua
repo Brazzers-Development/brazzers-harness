18ba76d3bc0b21437e0f494b12ddfd4790cdf3dc
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-10-31 09:27:25 -0400

    hasHarness check

--- locales/en.lua
+++ locales/en.lua
@@ -3,6 +3,7 @@ local Translations = {
         not_in_vehicle = 'You must be in a vehicle to install a harness',
         wrong_class = 'You cannot install a harness in this type of vehicle',
         canceled_installation = 'Canceled installation',
+        already_installed = 'You already have a harness installed'
     },
 }
 
--- server/server.lua
+++ server/server.lua
@@ -26,6 +26,7 @@ RegisterNetEvent('brazzers-harness:server:installHarness', function(plate)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
+    if hasHarness(plate) then return TriggerClientEvent('QBCore:Notify', src, Lang:t("error.already_installed"), 'error') end
     -- Remove Item
     Player.Functions.RemoveItem(Config.Harness, 1)
     TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items[Config.Harness], 'remove')
